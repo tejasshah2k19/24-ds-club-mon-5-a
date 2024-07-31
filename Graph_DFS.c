70f78a8014ba8851fe0d74caffad4f5f0ca24bf2
Tejas Shah <tejasshah2k19@gmail.com>
2024-07-31 17:49:48 +0530

    dfs

--- Graph_DFS.c
+++ Graph_DFS.c
@@ -11,30 +11,40 @@ int main()
 
     struct edge e[8];
     int i;
-    int visited=1; 
-    int start = 9; 
+    int visited[] = {0, 0, 0, 0}; // 0 1 2 3 4 5 6 7  -> 0
+    int src, dest;
+
+    int start = 9;
     for (i = 0; i < 8; i++)
     {
         printf("\nSrc - Dest");
-        scanf("%d%d", &e[i].src, &e[i].dest);
+        scanf("%d%d", &src, &dest);
+        e[i].src = src;
+        e[i].dest = dest;
+
+        i++;
+        e[i].src = dest;
+        e[i].dest = src;
     }
 
+    printf("\nEnter Inital Vertex");
+    scanf("%d", &start); // 0
 
-    printf("\n%d ",start);
-    for (i = 0; i < 8; i++)
+    printf("\n%d ", start); // 0
+    visited[start] = 1;
+
+    for (i = 0; i < 8; i++) // i:0
     {
-        if(visited == 4){
-            break; 
-        }
-        if(e[i].src == start ){
-            visited++;
-            printf(" %d",e[i].dest);
-            start = e[i].dest; 
-        }
 
+        if (e[i].src == start && visited[e[i].dest] == 0)
+        {
+            printf(" %d", e[i].dest); // 1
+            start = e[i].dest;        // 1
+            visited[start] = 1;  
+        }
     }
-        //9-> ?
 
+    // 9-> ?
 
     return 0;
 }
